b21db2bd9829c34df2762d823f6d12e923ba82da
Daniel Vidal <danielsv465@hotmail.com>
2023-01-23 08:54:38 -0300

    create controller

---
+++ src/jogadores/jogadores.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+
+@Controller('api/v1/jogadores')
+export class JogadoresController {
+  @Post()
+  async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
+    const { nome, email, telefoneCelular } = criarJogadorDto;
+    return JSON.stringify({
+      nome: 'daniel',
+    });
+  }
+}
